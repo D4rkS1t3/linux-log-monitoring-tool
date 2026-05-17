1940321a7c7127cd36ac688c9f36d719cf217135
D4rkS1t3 <140694059+D4rkS1t3@users.noreply.github.com>
2026-05-17 14:37:10 +0200

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -5,30 +5,30 @@ from collections import Counter
 error_patterns = ["ERROR", "timeout", "failed", "refused", "database"]
 
 def analyze_log(file_path):
-        errors = []
+    errors = []
 
-        try:
-                with open(file_path, "r") as f:
-                        for line in f:
-                                for pattern in error_patterns:
-                                        if pattern.lower() in line.lower():
-                                                errors.append(pattern)
-        except FileNotFoundError:
-                print(f"Blad: Plik '{file_path}' nie istnieje!")
-                return
+    try:
+        with open(file_path, "r") as f:
+            for line in f:
+                for pattern in error_patterns:
+                    if pattern.lower() in line.lower():
+                        errors.append(pattern)
+    except FileNotFoundError:
+        print(f"Blad: Plik '{file_path}' nie istnieje!")
+        return
 
-        summary = Counter(errors)
+    summary = Counter(errors)
 
-        with open("report.txt", "w") as f:
-                f.write("=====================================\n")
-                f.write("       LOG ANALYSIS REPORT\n")
-                f.write("=====================================\n\n")
-                for k, v in summary.items():
-                        f.write(f"Typ bledu: '{k}' -> znaleziono razy: {v}\n")
+    with open("report.txt", "w") as f:
+        f.write("=====================================\n")
+        f.write("       LOG ANALYSIS REPORT\n")
+        f.write("=====================================\n\n")
+        for k, v in summary.items():
+            f.write(f"Typ bledu: '{k}' -> znaleziono razy: {v}\n")
 
-        print(f"Raport z pliku '{file_path}' zostal pomyslnie wygnerowany w report.txt")
+    print(f"Raport z pliku '{file_path}' zostal pomyslnie wygnerowany w report.txt")
 
-penguin = """
+    penguin = """
      .-"-.
     / 0 0 \\
     \\_ v _/         LINUX LOG ANALYZER
@@ -41,13 +41,13 @@ penguin = """
 
 
 if __name__ == "__main__":
-        if len(sys.argv) < 2:
-                print("Blad: Nie podales plikou z logami!")
-                print("Uzycie: python3 analyzer.py <sciezka_do_pliku_z_logami>")
-                sys.exit(1)
+    if len(sys.argv) < 2:
+        print("Blad: Nie podales plikou z logami!")
+        print("Uzycie: python3 analyzer.py <sciezka_do_pliku_z_logami>")
+        sys.exit(1)
 
-        #pobieramy drugi argument z terminala podany do pythona czyli plik z logami
-        wybrany_plik = sys.argv[1]
+    # pobieramy drugi argument z terminala podany do pythona czyli plik z logami
+    wybrany_plik = sys.argv[1]
 
-        #uruchamiamy skrypt
-        analyze_log(wybrany_plik)
+    # uruchamiamy skrypt
+    analyze_log(wybrany_plik)
